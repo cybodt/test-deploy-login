a14272ffa0f5ede2b7e859ae6bd220d7fe7bd3b8
cybodt <cybodt@live.com>
2025-10-17 18:37:40 +0300

    regular commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Routes, Route } from 'react-router';
 import './App.css';
 
 function App() {
 	return (
-		<div className='app'>
-			<p>test</p>
-		</div>
+		<Routes>
+			<Route />
+		</Routes>
 	);
 }
 
