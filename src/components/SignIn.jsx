68e11dff8cd0e35d82a459b324e5a4b7a4c7efa4
cybodt <cybodt@live.com>
2025-12-15 14:47:50 +0200

    regular commit

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
+import './SignIn.css';
 
 function SignIn() {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<div className='sign-in__container'>
-			<div className='sign-in__greeting'>
-				<p>Sign in to your account</p>
-				<div>
+			<div className='sign-in__intro'>
+				<div className='sign-in__greeting-msg'>Sign in to your account</div>
+				<div className='sign-in__reg-msg'>
 					<div className='sign-in__reg-label'>New to eBay?</div>
 					<Link to='singuppersonal'>
-						<button type='button' className='sign-in__reg-btn'>
+						<button type='button' className='sign-in__create-account-btn-link'>
 							Create account
 						</button>
 					</Link>
 				</div>
 			</div>
+			<form onSubmit={handleSubmit} className='sign-in__form'></form>
 		</div>
 	);
 }
